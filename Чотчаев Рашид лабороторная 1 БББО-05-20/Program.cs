10f4e3d66c96cf613c6c25d056fbacf3edc39894
student <student@C320U06>
2022-02-10 13:18:09 +0300

    Добавьте файлы проекта.

---
+++ Чотчаев Рашид лабороторная 1 БББО-05-20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Чотчаев_Рашид_лабороторная_1_БББО_05_20
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
